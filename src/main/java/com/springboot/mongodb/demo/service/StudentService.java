4bf8ba3432f09ad6c5c63487e33605d1f23aed3e
SHUBHAM KUMAR CHOUDHARY <shubhamchoudhary7114@gmail.com>
2023-05-29 14:03:39 +0530

    removed extra space

--- src/main/java/com/springboot/mongodb/demo/service/StudentService.java
+++ src/main/java/com/springboot/mongodb/demo/service/StudentService.java
@@ -7,21 +7,12 @@ import java.util.List;
 
 public interface StudentService {
     public List<Student> getAllStudent();
-
     public Student findById(String theId);
-
     public Student saveStudent(Student theStudent);
-
     public ResponseEntity<String> deleteById(String theId);
-
     public Student updateStudent(Student theStudent);
-
     public List<Student> getStudentsByName(String studentName);
     public List<Student> getStudentsByNameAndEmail(String studentName ,  String email);
     public List<Student> getStudentByPagination(int pageNo ,  int pageSize);
-
     public List<Student> getStudentBySorting();
-
-
-
 }
